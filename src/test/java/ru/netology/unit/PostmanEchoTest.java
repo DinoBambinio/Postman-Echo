275d5f9c30fcee5d103022fafe06ec414725cf7f
DinoBambinio <117571267+DinoBambinio@users.noreply.github.com>
2023-04-13 13:00:48 +0300

    dataTest

--- src/test/java/ru/netology/unit/PostmanEchoTest.java
+++ src/test/java/ru/netology/unit/PostmanEchoTest.java
@@ -12,14 +12,14 @@ public class PostmanEchoTest {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("1") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("02/15/2023") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("1"))
+                .body("data",equalTo("02/15/2024"))
         ;
     }
 }
